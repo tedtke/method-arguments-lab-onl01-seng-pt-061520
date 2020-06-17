57609c80b1b1a912bedad96918dc9206248dc263
Teodoro Garcia III <tedtke514@gmail.com>
2020-06-17 00:45:05 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,3 @@
-def introduction_with_language(name, language)
-  return "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(Dan, Ember)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
